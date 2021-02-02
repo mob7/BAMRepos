9fee0f6c1cfc4377ce1ddef4a66aa9f085256073
Oussama El Belaidi <mohamedoussama.elbeladi@smile-maroc.com>
2021-02-02 23:40:06 +0100

    Add pull to refresh

--- app/src/main/java/com/mob/bamrepos/repositories/MainRepo.kt
+++ app/src/main/java/com/mob/bamrepos/repositories/MainRepo.kt
@@ -1,6 +1,5 @@
 package com.mob.bamrepos.repositories
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.mob.bamrepos.models.response.Repo
@@ -11,10 +10,9 @@ import retrofit2.Response
 import javax.inject.Inject
 
 class MainRepo @Inject constructor(
-    private var apiService: ApiService
+    private var apiService: ApiService,
 ) {
 
-    private val TAG = "BAMRepos"
     private val _dataResponseLD = MutableLiveData<List<Repo>>()
 
     val dataResponseLD: LiveData<List<Repo>>
@@ -24,11 +22,14 @@ class MainRepo @Inject constructor(
         val call = apiService.getRepos()
         call.enqueue(object : Callback<List<Repo>> {
             override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-                _dataResponseLD.postValue(response.body())
+                if (response.isSuccessful && response.body() != null)
+                    _dataResponseLD.postValue(response.body())
+                else
+                    _dataResponseLD.postValue(emptyList())
             }
 
             override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-                Log.e(TAG, "onFailure: " + t.message)
+                _dataResponseLD.postValue(emptyList())
             }
         })
     }
--- app/src/main/java/com/mob/bamrepos/ui/MainFragment.kt
+++ app/src/main/java/com/mob/bamrepos/ui/MainFragment.kt
@@ -2,17 +2,19 @@ package com.mob.bamrepos.ui
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.mob.bamrepos.R
 import com.mob.bamrepos.viewmodels.MainViewModel
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_main.*
 import javax.inject.Inject
 
-class MainFragment : Fragment(R.layout.fragment_main) {
+class MainFragment : Fragment(R.layout.fragment_main), SwipeRefreshLayout.OnRefreshListener {
 
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
@@ -35,10 +37,25 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
         mViewModel.dataResponseLD.observe(viewLifecycleOwner,
             Observer {
-                mMainAdapter.updateData(it)
+                if (it.isEmpty()) {
+                    Toast.makeText(context, "Une erreur est survenue !", Toast.LENGTH_SHORT).show()
+                } else {
+                    mMainAdapter.updateData(it)
+                }
                 loadingProgressBar.visibility = View.INVISIBLE
+                swipeRefresh.isRefreshing = false
             })
 
+        swipeRefresh.setOnRefreshListener(this)
+        swipeRefresh.post {
+            swipeRefresh.isRefreshing = true
+            mViewModel.getRepos()
+        }
+    }
+
+    override fun onRefresh() {
+        swipeRefresh.isRefreshing = true
+        mViewModel.getRepos()
     }
 
 }
